fe094eae0d682cad9f369a96a0031995d6c1e8a3
rizafrivaldi <rizafrivaldi@gmail.com>
2025-09-09 21:41:35 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Operasi Dasar pada Array
+let laptop = ["LENOVO", "MSI", "ASUS"];
+console.log(laptop[0]);
+console.log(laptop[1]);
+
+// Tambah elemen di akhir
+laptop.push("ACER");
+console.log(laptop);
+
+// Hapus elemen terakhir
+laptop.pop();
+console.log(laptop);
+
+// Tambah di awal
+laptop.unshift("AXIOO");
+console.log(laptop);
+
+// Hapus di awal
+laptop.shift();
+console.log(laptop);
+
+// Panjang array
+console.log(laptop.length);
+
+// Looping
